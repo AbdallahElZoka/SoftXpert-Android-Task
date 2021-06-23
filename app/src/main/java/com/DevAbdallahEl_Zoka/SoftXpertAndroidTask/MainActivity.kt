d4292aa2903e576479a7d8c8d1cd3dcfdeee0710
Abdallah El-Zoka <abdallah.elzoka@gmail.com>
2021-06-23 14:11:37 +0200

    Initial Commit

---
+++ app/src/main/java/com/DevAbdallahEl_Zoka/SoftXpertAndroidTask/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.DevAbdallahEl_Zoka.SoftXpertAndroidTask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
